bb8aa64515c8e9e27d693cf08ff2cd055fcfcf0c
SterlingYM <sterling.astro@berkeley.edu>
2025-06-10 10:07:24 -0400

    improve sim load

--- atlast/__init__.py
+++ atlast/__init__.py
@@ -1,2 +1,2 @@
 from .api_ATLAS import QueryATLAS, AtlasPhotometry, AtlasBinnedPhotometry, init_sncosmo_filters
-from .api_SNANA import SNANAPhotometry, read_from_sim
+from .api_SNANA import SNANAPhotometry, SNANASim
